f232a5ffaae99b9525a888b9f956dfc51af5f37b
kgitbank <kgitbank@DESKTOP-U6AB831>
2018-09-11 09:39:49 +0900

    완성

--- src/handlers/CookieHandler.java
+++ src/handlers/CookieHandler.java
@@ -1,17 +1,21 @@
 package handlers;
 
+import java.util.Map;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import beans.LoginLogDao;
 
 public class CookieHandler implements ServletRequestListener {
-
+	@Override
 	public void requestInitialized(ServletRequestEvent sre) {
 		ServletRequest req = sre.getServletRequest();
 		HttpServletRequest request = (HttpServletRequest) req;
-
 		if (request.getSession().isNew()) {
 			Cookie[] ar = request.getCookies();
 			String freepass = null;
@@ -22,12 +26,28 @@ public class CookieHandler implements ServletRequestListener {
 					}
 				}
 			}
+			// ===================================================================
 			if (freepass != null) {
-				System.out.println("[CookieHandler] this client has a freepass cookie. value is" + freepass);
-				request.getSession().setAttribute("auth", true);
+				System.out.println("[CookieHandler] this client has a freepass cookie. value is " + freepass);
+				HttpSession session= request.getSession();
+				session.setAttribute("auth", true);
+				session.setAttribute("logid", freepass);
+				
+				
+				LoginLogDao ldao = new LoginLogDao();
+				Map log = ldao.getLatesetLogById(freepass);
+				if (log != null) {
+					session.setAttribute("latest", log.get("time"));
+				}
+
+				ldao.addLog(freepass);
+
 			} else {
 				System.out.println("[CookieHandler] this client does not have a freepass cookie");
 			}
 		}
 	}
+
 }
+
+
